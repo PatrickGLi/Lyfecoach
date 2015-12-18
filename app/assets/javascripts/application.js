fc8f65e2f8fd78c5ef503b5888f7698cfa7d88b0
Patrick Li <patrickli727@gmail.com>
2015-12-18 13:24:35 -0800

    Adding filtered searches.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap.min
+//= require bootstrap
 //= require_tree .
 //= require bundle
--- frontend/components/filter/filter.jsx
+++ frontend/components/filter/filter.jsx
@@ -1,22 +1,83 @@
 var React = require('react'),
+    ReactDOM = require('react-dom'),
+    _ = require('underscore-node'),
     DropdownActions = require('../../actions/dropdown_actions'),
+    FilterParamsStore = require('../../stores/filter_params_store'),
+    FilterActions = require('../../actions/filter_actions'),
     DropdownStore = require('../../stores/dropdown_store'),
+    SearchFilter = require('./search_filter'),
     PriceFilter = require('./price'),
     CategoryFilter = require('./category'),
     EventTypeFilter = require('./event_type'),
     DateFilter = require('./date');
 
+function _getFilterParams() {
+  return FilterParamsStore.params();
+}
+
 var Filter = React.createClass({
   getInitialState: function() {
-    return({ shown: "" });
+    return({
+      shown: "",
+      filterParams: _getFilterParams()
+    });
   },
 
   componentDidMount: function() {
+    var autoCompleteInput = ReactDOM.findDOMNode(this.refs.autocomplete);
+
+    this.autocomplete = new google.maps.places.Autocomplete(autoCompleteInput,
+      {types: ['geocode']});
+    this.geocoder = new google.maps.Geocoder();
+    this.autocomplete.addListener('place_changed', this.searchByLocation);
+
     this.token = DropdownStore.addListener(this._onChange);
   },
 
   componentWillUnmount: function() {
     this.token.remove();
+  },
+
+  searchByLocation: function() {
+    var that = this;
+    var place = this.autocomplete.getPlace();
+        address = place.formatted_address;
+        debugger
+    this.geocoder.geocode({ 'address' : address}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        var myLatLng = results[0].geometry.location;
+        this.lat = myLatLng.lat();
+        this.lng = myLatLng.lng();
+
+        FilterActions.fetchEventByLocation(locationData);
+      } else {
+        alert(status);
+      }
+    });
+  },
+
+  geolocate: function() {
+    var that = this;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var geolocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        var circle = new google.maps.Circle({
+          center: geolocation,
+          radius: position.coords.accuracy
+        });
+
+        that.autocomplete.setBounds(circle.getBounds());
+      });
+    }
+  },
+
+  handleSubmit: function(event){
+      debugger
+    event.preventDefault();
+
   },
 
   handleClick: function(e) {
@@ -27,13 +88,26 @@ var Filter = React.createClass({
     this.setState({ shown: DropdownStore.fetch() });
   },
 
+  handleChange: function(e) {
+    this.setState({ location: e.target.value})
+  },
+
   render: function() {
     return (
       <div>
-        <PriceFilter toggle={this.state.shown} onClick={this.handleClick}/><br/>
-        <CategoryFilter toggle={this.state.shown} onClick={this.handleClick}/><br/>
-        <EventTypeFilter toggle={this.state.shown} onClick={this.handleClick}/><br/>
-        <DateFilter toggle={this.state.shown} onClick={this.handleClick}/>
+        <SearchFilter filterParams={this.state.filterParams}/>
+        <PriceFilter toggle={this.state.shown}
+                     onClick={this.handleClick}
+                     filterParams={this.state.filterParams}/><br/>
+        <CategoryFilter toggle={this.state.shown}
+                        onClick={this.handleClick}
+                        filterParams={this.state.filterParams}/><br/>
+        <EventTypeFilter toggle={this.state.shown}
+                         onClick={this.handleClick}
+                         filterParams={this.state.filterParams}/><br/>
+        <DateFilter toggle={this.state.shown}
+                    onClick={this.handleClick}
+                    filterParams={this.state.filterParams}/>
       </div>
     );
   }
