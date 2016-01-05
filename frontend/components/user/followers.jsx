e7c47379eb47711a7302d9e0cafcddfaaaba95a7
Patrick Li <patrickli727@gmail.com>
2016-01-05 14:33:15 -0800

    Added photograph to followers list.

--- frontend/components/user/followers.jsx
+++ frontend/components/user/followers.jsx
@@ -26,9 +26,11 @@ var UserFollowers = React.createClass({
     var goToUser = this.goToUser;
     var followers = this.state.followers.map(function(follower){
       var bindedClick = goToUser.bind(null, follower);
+      var image = "http://res.cloudinary.com/dlqjek68b/image/upload/c_fill,h_50,w_50" + follower.follower.url;
       return <li onClick={bindedClick}
                  key={follower.follower.id}>
-                 {follower.  follower.host_name}
+                 <img src={image}></img>
+                 <div className="follower-text">{follower.follower.host_name}</div>
              </li>
     });
 
